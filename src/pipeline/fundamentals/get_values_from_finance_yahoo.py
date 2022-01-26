09a399c7a8adbc99b9e7ad8355b3ae7c0d84cc94
almarca4 <marcaralexander@gmail.com>
2022-01-26 20:49:43 +0100

    gitignore

---
+++ src/pipeline/fundamentals/get_values_from_finance_yahoo.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+import yfinance as yf
+import plotly.express as px
+import plotly.graph_objects as go
